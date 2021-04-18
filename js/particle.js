de7207101009177017161e4b9c319edd3231a45f
Anvay P <anvay13@gmail.com>
2021-04-18 17:56:36 +0530

    page index changed

--- js/particle.js
+++ js/particle.js
@@ -1,9 +1,29 @@
 // modified version of random-normal
-function normalPool(o){var r=0;do{var a=Math.round(normal({mean:o.mean,dev:o.dev}));if(a<o.pool.length&&a>=0)return o.pool[a];r++}while(r<100)}function randomNormal(o){if(o=Object.assign({mean:0,dev:1,pool:[]},o),Array.isArray(o.pool)&&o.pool.length>0)return normalPool(o);var r,a,n,e,l=o.mean,t=o.dev;do{r=(a=2*Math.random()-1)*a+(n=2*Math.random()-1)*n}while(r>=1);return e=a*Math.sqrt(-2*Math.log(r)/r),t*e+l}
+function normalPool(o) {
+    var r = 0;
+    do {
+        var a = Math.round(normal({mean: o.mean, dev: o.dev}));
+        if (a < o.pool.length && a >= 0) return o.pool[a];
+        r++
+    } while (r < 100)
+}
 
-const NUM_PARTICLES = 600;
+function randomNormal(o) {
+    if (o = Object.assign({
+        mean: 0,
+        dev: 1,
+        pool: []
+    }, o), Array.isArray(o.pool) && o.pool.length > 0) return normalPool(o);
+    var r, a, n, e, l = o.mean, t = o.dev;
+    do {
+        r = (a = 2 * Math.random() - 1) * a + (n = 2 * Math.random() - 1) * n
+    } while (r >= 1);
+    return e = a * Math.sqrt(-2 * Math.log(r) / r), t * e + l
+}
+
+const NUM_PARTICLES = 400;
 const PARTICLE_SIZE = 0.5; // View heights
-const SPEED = 20000; // Milliseconds
+const SPEED = 40000; // Milliseconds
 
 let particles = [];
 
@@ -13,18 +33,18 @@ function rand(low, high) {
 
 function createParticle(canvas) {
     const colour = {
-        r:  randomNormal({ mean: 125, dev: 20 }),
-        g: randomNormal({ mean: 125, dev: 120 }),
-        b:  randomNormal({ mean: 125, dev: 20 }),
+        r: randomNormal({mean: 125, dev: 20}),
+        g: randomNormal({mean: 125, dev: 120}),
+        b: randomNormal({mean: 125, dev: 20}),
         a: rand(0, 1),
     };
     return {
         x: -2,
         y: -2,
-        diameter: Math.max(0, randomNormal({ mean: PARTICLE_SIZE, dev: PARTICLE_SIZE / 2 })),
-        duration: randomNormal({ mean: SPEED, dev: SPEED * 0.1 }),
-        amplitude: randomNormal({ mean: 16, dev: 2 }),
-        offsetY: randomNormal({ mean: 0, dev: 10 }),
+        diameter: Math.max(0, randomNormal({mean: PARTICLE_SIZE, dev: PARTICLE_SIZE / 2})),
+        duration: randomNormal({mean: SPEED, dev: SPEED * 0.1}),
+        amplitude: randomNormal({mean: 16, dev: 2}),
+        offsetY: randomNormal({mean: 0, dev: 10}),
         arc: Math.PI * 2,
         startTime: performance.now() - rand(0, SPEED),
         colour: `rgba(${colour.r}, ${colour.g}, ${colour.b}, ${colour.a})`,
@@ -103,12 +123,13 @@ function startAnimation() {
 };
 
 // Start animation when document is loaded
-(function () {
-    if (document.readystate !== 'loading') {
-        startAnimation();
-    } else {
-        document.addEventListener('DOMContentLoaded', () => {
+setTimeout(
+    function () {
+        if (document.readystate !== 'loading') {
             startAnimation();
-        })
-    }
-}());
+        } else {
+            document.addEventListener('DOMContentLoaded', () => {
+                startAnimation();
+            })
+        }
+    }, 1000);
